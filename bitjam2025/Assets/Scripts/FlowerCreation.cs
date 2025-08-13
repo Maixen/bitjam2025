9d58a62ccd1d6a9e80b69a0ed8a75488c52edff2
Glueckblubbert <glueckblubbert.gamer@gmail.com>
2025-08-13 11:37:55 +0200

    Added Custom Flowers Generation

---
+++ bitjam2025/Assets/Scripts/FlowerCreation.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerCreation : MonoBehaviour
+{
+    [SerializeField] private GameObject flowerPrefab;
+    [SerializeField] private Vector2 spawnPos;
+    [Space]
+    [SerializeField] private List<Sprite> vaseGood;
+    [SerializeField] private List<Sprite> stemGood;
+    [SerializeField] private List<Sprite> leafGood;
+    [SerializeField] private List<Sprite> blossomGood;
+    [Space]
+    [SerializeField] private List<Sprite> vaseBad;
+    [SerializeField] private List<Sprite> stemBad;
+    [SerializeField] private List<Sprite> leafBad;
+    [SerializeField] private List<Sprite> blossomBad;
+    [Space]
+    [SerializeField, Range(0f,1f)] private float chanceOfBadFlower;
+    [SerializeField, Range(1,3)] private int maxBadFlowerParts;
+
+    public void CreateFlower()
+    {
+        print("Started flower printing");
+        bool[] badParts = new bool[5];
+        if (CheckIfBad(Random.Range(0f, 1f)))
+            badParts = DistributeBadParts(Random.Range(1,maxBadFlowerParts + 1));
+        List<Sprite> flowerSprites = GetRandomFlowerSprites(badParts);
+        GameObject newFlower = Instantiate(flowerPrefab, spawnPos, Quaternion.identity);
+        newFlower.GetComponent<FlowerData>().GetFlowerData(flowerSprites, badParts);
+        print("Flower was printed");
+    }
+
+
+    List<Sprite> GetRandomFlowerSprites(bool[] isBadPart)
+    {
+        List<Sprite> sprites = new List<Sprite>();
+        if (isBadPart[0])
+            sprites.Add(GetRandomSpriteFromList(ref vaseBad));
+        else
+            sprites.Add(GetRandomSpriteFromList(ref vaseGood));
+        if (isBadPart[1])
+            sprites.Add(GetRandomSpriteFromList(ref stemBad));
+        else
+            sprites.Add(GetRandomSpriteFromList(ref stemGood));
+        if (isBadPart[2])
+            sprites.Add(GetRandomSpriteFromList(ref leafBad));
+        else
+            sprites.Add(GetRandomSpriteFromList(ref leafGood));
+        if (isBadPart[3])
+            sprites.Add(GetRandomSpriteFromList(ref leafBad));
+        else
+            sprites.Add(GetRandomSpriteFromList(ref leafGood));
+        if (isBadPart[4])
+            sprites.Add(GetRandomSpriteFromList(ref blossomBad));
+        else
+            sprites.Add(GetRandomSpriteFromList(ref blossomGood));
+        return sprites;
+    }
+
+    Sprite GetRandomSpriteFromList(ref List<Sprite> list)
+    {
+        return list[Random.Range(0, list.Count)];
+    }
+
+    bool[] DistributeBadParts(int amount)
+    {
+        int badPartsInserted = 0;
+        bool[] parts = new bool[5];
+        while (badPartsInserted < amount)
+        {
+            int partToBeGottenBad = Random.Range(0,parts.Length);
+            if (parts[partToBeGottenBad] == true)
+                continue;
+            parts[partToBeGottenBad] = true;
+            badPartsInserted++;
+        }
+        return parts;
+    }
+
+
+    bool CheckIfBad(float randomNumber)
+    {
+        if (randomNumber < chanceOfBadFlower)
+            return true;
+        return false;
+    }
+
+}
---
+++ bitjam2025/Assets/Scripts/FlowerData.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerData : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer vaseSprite;
+    [SerializeField] private SpriteRenderer stemSprite;
+    [SerializeField] private SpriteRenderer leafLSprite;
+    [SerializeField] private SpriteRenderer leafRSprite;
+    [SerializeField] private SpriteRenderer blossomSprite;
+    [Space]
+    [SerializeField] private bool[] partStatuses;
+
+    public void GetFlowerData(List<Sprite> flowerSprites, bool[] parts)
+    {
+        partStatuses = parts;
+        SetFlowerPartSprite(ref vaseSprite, flowerSprites[0]);
+        SetFlowerPartSprite(ref stemSprite, flowerSprites[1]);
+        SetFlowerPartSprite(ref leafLSprite, flowerSprites[2]);
+        SetFlowerPartSprite(ref leafRSprite, flowerSprites[3]);
+        SetFlowerPartSprite(ref blossomSprite, flowerSprites[4]);
+    }
+
+    private void SetFlowerPartSprite(ref SpriteRenderer sR, Sprite newSprite)
+    {
+        sR.sprite = newSprite;
+    }
+
+    public bool[] GetBadParts()
+    {
+        return partStatuses;
+    }
+}
