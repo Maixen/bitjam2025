05f2f43d33c4cb763e6f5f3a8ed771bbfaf7526d
Glueckblubbert <glueckblubbert.gamer@gmail.com>
2025-08-14 21:27:08 +0200

    Parasite Minigame

--- bitjam2025/Assets/Scripts/Minigames/ParasiteMinigame.cs
+++ bitjam2025/Assets/Scripts/Minigames/ParasiteMinigame.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class ParasiteMinigame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject bugPrefab;
+    [Space]
+    [SerializeField] private bool playable;
+    [SerializeField] private bool gameHasStarted;
+    [SerializeField] private Vector2 bugSpawnAmountRange;
+    [SerializeField] private int bugSpawnCount;
+    [SerializeField] private int bugsLeft;
+    [SerializeField] private Vector2 bugSpawnTimeRange;
+    [SerializeField] private float nextBugSpawnsIn;
+    [SerializeField] private Vector2 xBugSpawnBoundaries;
+    [SerializeField] private Vector2 yBugSpawnBoundaries;
+
+    //<3
+    private float RandomizeFromRange(Vector2 range)
     {
-        
+        return Random.Range(range.x, range.y);
+    } //<3
+
+    public void IsPlayable()
+    {
+        playable = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartMinigame()
     {
-        
+        if (gameHasStarted || !playable)
+            return;
+        gameHasStarted = true;
+        bugSpawnCount = (int)RandomizeFromRange(bugSpawnAmountRange);
+        SpawnBug(new Vector2(RandomizeFromRange(xBugSpawnBoundaries), RandomizeFromRange(yBugSpawnBoundaries)));
     }
+
+    private void Update()
+    {
+        if (bugSpawnCount == 0)
+            return;
+        nextBugSpawnsIn -= Time.deltaTime;
+        if (nextBugSpawnsIn > 0)
+            return;
+        SpawnBug(new Vector2(RandomizeFromRange(xBugSpawnBoundaries), RandomizeFromRange(yBugSpawnBoundaries)));
+    }
+
+    public void BugWasSquashed()
+    {
+        if (bugsLeft-- == 0 && bugSpawnCount == 0)
+            Win();
+    }
+
+    private float ResetBugSpawnTimer()
+    {
+        return RandomizeFromRange(bugSpawnTimeRange);
+    }
+
+    void SpawnBug(Vector2 spawnPos)
+    {
+        GameObject newBug = Instantiate(bugPrefab, spawnPos, Quaternion.Euler(0, 0, Random.Range(0, 360)));
+        newBug.GetComponent<BugBehaviour>().minigameParent = this;
+        bugSpawnCount--;
+        bugsLeft++;
+        nextBugSpawnsIn = ResetBugSpawnTimer();
+    }
+
+    private void Win()
+    {
+        //Game is won, duh
+        gameHasStarted = false;
+        gameObject.SetActive(false);
+        playable = false;
+    }
+    
 }
