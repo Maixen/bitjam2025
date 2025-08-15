5ad40e13c6e2d1bd4a7ee832dfa0525fd6e2597a
Glueckblubbert <glueckblubbert.gamer@gmail.com>
2025-08-15 19:31:05 +0200

    MoneyDisplayScript

--- bitjam2025/Assets/Scripts/SelfmadeUI/MoneyDisplayScript.cs
+++ bitjam2025/Assets/Scripts/SelfmadeUI/MoneyDisplayScript.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class MoneyDisplayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<SpriteRenderer> numberRenderers;
+    [SerializeField] private List<Sprite> numberSprites;
+    [SerializeField] private int number;
+    [SerializeField] private int numberOnDisplay;
+    [SerializeField] private int numberCountUpsPerSecond;
+    [SerializeField] private float countUpPercent;
+    private float untilNextCount;
+
+    public void SetNewValue(int newValue)
     {
-        
+        number = newValue;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int GetValue()
     {
-        
+        return number;
+    }
+
+    private void Update()
+    {
+        if (number == numberOnDisplay)
+            return;
+        if(untilNextCount < 1/numberCountUpsPerSecond)
+        {
+            untilNextCount += Time.deltaTime;
+            return;
+        }
+        numberOnDisplay += (int)((number - numberOnDisplay) * countUpPercent);
+        if(numberOnDisplay > number)
+            numberOnDisplay = number;
+        GetSpritesFromNumber();
+    }
+
+    private void GetSpritesFromNumber()
+    {
+        for(int i = 0; i < numberRenderers.Count; i++)
+        {
+            int moduluRest = (numberOnDisplay / (int)Mathf.Pow(numberOnDisplay,i)) % 10;
+            if(moduluRest > 9)
+            {
+                print("Ich hatte leider unrecht, mein größtes Beileid Herr Marksen");
+                moduluRest = 9;
+            }
+            numberRenderers[i].sprite = numberSprites[moduluRest];
+        }
     }
 }
