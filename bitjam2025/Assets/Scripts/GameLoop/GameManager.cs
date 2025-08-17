a1213398e420231a17ca813c41b0fa7b7917b0cd
Maixen <maixen@gmx.de>
2025-08-18 00:05:26 +0200

    webgl fix and info fix

--- bitjam2025/Assets/Scripts/GameLoop/GameManager.cs
+++ bitjam2025/Assets/Scripts/GameLoop/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
